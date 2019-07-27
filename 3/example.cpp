6eb5c13fb0b3071c41143004d65c00c47f55d943
iloveooz <iloveooz@yandex.ru>
2019-07-27 22:03:46 +0700

    some changes

--- 3/example.cpp
+++ 3/example.cpp
@@ -11,16 +11,35 @@
 	 
 		sf::Sprite herosprite;
 		herosprite.setTexture(herotexture);
+		herosprite.setTextureRect(sf::IntRect(0, 192, 96, 96));
 		herosprite.setPosition(50, 25);
 	 
-		while (window.isOpen())	
-		{
+		while (window.isOpen())	{
 			sf::Event event;
-			while (window.pollEvent(event))
-			{
+			while (window.pollEvent(event)) {
 				if (event.type == sf::Event::Closed)
 					window.close();
 			}
+			
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+				herosprite.move(-0.1, 0);
+				herosprite.setTextureRect(sf::IntRect(0, 96, 96, 96));
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+				herosprite.move(0.1, 0);
+				herosprite.setTextureRect(sf::IntRect(0, 192, 96, 96));
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+				herosprite.move(0, -0.1);
+				herosprite.setTextureRect(sf::IntRect(0, 288, 96, 96));
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) || sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+				herosprite.move(0, 0.1);
+				herosprite.setTextureRect(sf::IntRect(0, 0, 96, 96));
+			}
+	 
+			if (sf::Mouse::isButtonPressed(sf::Mouse::Right)) 
+				herosprite.setColor(sf::Color::Red);
 	 
 			window.clear();
 			window.draw(herosprite);
